ba34a9c9c2605e424fadc368e25d768afe0474d1
shubham7jain <shubham7jain@gmail.com>
2018-02-03 12:23:02 -0600

    Changesush

--- application.py
+++ application.py
@@ -53,8 +53,8 @@ def editGroup():
 
         postId = json_dict["postid"]
         courseNumber = json_dict["courseNumber"]
-        startDate = datetime.date.strftime(datetime.datetime.strptime(json_dict["startDate"], '%Y-%m-%d %H:%M'), '%Y-%m-%d %H:%M')
-        endDate = datetime.date.strftime(datetime.datetime.strptime(json_dict["endDate"], '%Y-%m-%d %H:%M'), '%Y-%m-%d %H:%M')
+        startDate = datetime.date.strftime(datetime.datetime.strptime(json_dict["startDate"], '%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M')
+        endDate = datetime.date.strftime(datetime.datetime.strptime(json_dict["endDate"], '%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M')
         location = json_dict["location"]
         capacity = int(json_dict["capacity"])
         contact = json_dict["contact"]
