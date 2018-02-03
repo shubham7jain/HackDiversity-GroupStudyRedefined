6e4bd0915f8d05cbde9f71f2333245fd812c2b93
shubham7jain <shubham7jain@gmail.com>
2018-02-02 20:59:14 -0600

    Added 2 apis - createGroup and getGroups

--- application.py
+++ application.py
@@ -1,23 +1,15 @@
 from flask import Flask, request
 import json
 import mysql.connector
+import uuid
+from bson import json_util
+import datetime
 
 application = Flask(__name__)
 
 @application.route('/createGroup', methods=['POST'])
 def createGroup():
     if request.method == "POST":
-    	cnx = mysql.connector.connect(user='smalltigerson', password='123456',
-                              host='database2.cs.tamu.edu',
-                              database='smalltigerson')
-    	cursor = cnx.cursor()
-
-    	query = ("SELECT uin FROM Groups ")
-
-    	cursor.execute(query)
-
-    	for (uin) in cursor:
-    		print(uin)
 
     	json_dict = json.loads(request.data)
     	uin = json_dict["uin"]
@@ -26,6 +18,48 @@ def createGroup():
     	startDate = json_dict["startDate"]
     	endDate = json_dict["endDate"]
     	location = json_dict["location"]
+    	capacity = json_dict["capacity"]
+
+    	cnx = mysql.connector.connect(user='smalltigerson', password='123456',
+                              host='database2.cs.tamu.edu',
+                              database='smalltigerson')
+    	cursor = cnx.cursor()
+
+    	createGroup = ("INSERT INTO Groups "
+               "(postid, course, name, uin, startTime, endTime, location, capacity) "
+               "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
+
+    	data = (uuid.uuid4().hex, courseNumber, name, uin, startDate, endDate, location, capacity)
+
+    	cursor.execute(createGroup, data)
+
+    	cnx.commit()
     	cursor.close()
     	cnx.close()
-    	return json.dumps('{"uin":' + uin + '}')
+    	return json.dumps('{"message": "Group is created successfully."}')
+
+@application.route('/getGroups')
+def getAllPosts():
+    cnx = mysql.connector.connect(user='smalltigerson', password='123456',
+                              host='database2.cs.tamu.edu',
+                              database='smalltigerson')
+    cursor = cnx.cursor()
+
+    query = ("SELECT course, name, uin, startTime, endTime, location from Groups")
+
+    cursor.execute(query)
+
+    result = []
+    for (course, name, uin, startTime, endTime, location) in cursor:
+    	result.append({
+    		"course": course,
+    		"name": name,
+    		"uin": uin,
+    		"startTime": str(startTime),
+    		"endTime": str(endTime),
+    		"location": location
+    		})
+
+    cursor.close()
+    cnx.close()
+    return json.dumps(result)
