afbd2738ac41350d2c3b5d2fe4d800cd83817f88
shubham7jain <shubham7jain@gmail.com>
2018-02-03 10:07:04 -0600

    Fixing get api

--- application.py
+++ application.py
@@ -15,34 +15,34 @@ CORS(application)
 def createGroup():
     if request.method == "POST":
 
-    	json_dict = json.loads(request.data)
+        json_dict = json.loads(request.data)
 
-    	uin = json_dict["uin"]
-    	name = json_dict["name"]
-    	courseNumber = json_dict["courseNumber"]
-    	startDate = datetime.date.strftime(datetime.datetime.strptime(json_dict["startDate"], '%m/%d/%Y %I:%M %p'), '%Y-%m-%d %H:%M')
-    	endDate = datetime.date.strftime(datetime.datetime.strptime(json_dict["endDate"], '%m/%d/%Y %I:%M %p'), '%Y-%m-%d %H:%M')
-    	location = json_dict["location"]
-    	capacity = json_dict["capacity"]
+        uin = json_dict["uin"]
+        name = json_dict["name"]
+        courseNumber = json_dict["courseNumber"]
+        startDate = datetime.date.strftime(datetime.datetime.strptime(json_dict["startDate"], '%m/%d/%Y %I:%M %p'), '%Y-%m-%d %H:%M')
+        endDate = datetime.date.strftime(datetime.datetime.strptime(json_dict["endDate"], '%m/%d/%Y %I:%M %p'), '%Y-%m-%d %H:%M')
+        location = json_dict["location"]
+        capacity = json_dict["capacity"]
 
-    	cnx = mysql.connector.connect(user='shubham7jain', password='mikeliu',
+        cnx = mysql.connector.connect(user='shubham7jain', password='mikeliu',
                       host='db4free.net',
                       port=3307,
                       database='student_groups')
-    	cursor = cnx.cursor()
+        cursor = cnx.cursor()
 
-    	createGroup = ("INSERT INTO `Groups` "
+        createGroup = ("INSERT INTO `Groups` "
                "(`postid`, `course`, `name`, `uin`, `startTime`, `endTime`, `location`, `capacity`) "
                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)")
 
-    	data = (uuid.uuid4().hex, courseNumber, name, uin, startDate, endDate, location, capacity)
+        data = (uuid.uuid4().hex, courseNumber, name, uin, startDate, endDate, location, capacity)
 
-    	cursor.execute(createGroup, data)
+        cursor.execute(createGroup, data)
 
-    	cnx.commit()
-    	cursor.close()
-    	cnx.close()
-    	return json.dumps({"message": "Group is created successfully."})
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        return json.dumps({"message": "Group is created successfully."})
 
 @application.route('/getGroups')
 def getAllPosts():
@@ -65,16 +65,16 @@ def getAllPosts():
         current = current.replace(tzinfo=None)
         print(current)
         if(end > current):
-        	result.append({
+            result.append({
                 "postid": postid,
-        		"course": course,
-        		"name": name,
-        		"uin": uin,
-        		"startTime": str(startTime),
-        		"endTime": str(endTime),
-        		"location": location,
+                "course": course,
+                "name": name,
+                "uin": uin,
+                "startTime": str(startTime),
+                "endTime": str(endTime),
+                "location": location,
                 "capacity": capacity
-        		})
+                })
 
     cursor.close()
     cnx.close()
@@ -84,31 +84,30 @@ def getAllPosts():
 def joinGroup():
     if request.method == "POST":
 
-    	json_dict = json.loads(request.data)
+        json_dict = json.loads(request.data)
         postId = json_dict["postid"]
 
-
-        if(json_dict["capacity"] == 0)
+        if(int(json_dict["capacity"]) == 0):
             return json.dumps('{"message": "Capacity is full!"}')
 
-    	cnx = mysql.connector.connect(user='shubham7jain', password='mikeliu',
+        cnx = mysql.connector.connect(user='shubham7jain', password='mikeliu',
                       host='db4free.net',
                       port=3307,
                       database='student_groups')
-    	cursor = cnx.cursor()
+        cursor = cnx.cursor()
 
-    	createGroup = ("UPDATE `Groups`"
+        createGroup = ("UPDATE `Groups`"
                         "SET `capacity` = `capacity` - 1"
                         "WHERE `postid` = %s")
 
-    	data = (postId)
+        data = (postId)
 
-    	cursor.execute(createGroup, data)
+        cursor.execute(createGroup, data)
 
-    	cnx.commit()
-    	cursor.close()
-    	cnx.close()
-    	return json.dumps('{"message": "Group is joint successfully."}')
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        return json.dumps('{"message": "Group is joint successfully."}')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 33507))
